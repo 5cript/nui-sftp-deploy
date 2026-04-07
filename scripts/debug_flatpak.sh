e4165142ac17bf888ecc39881cd67dacb4720977
5cript <tim06tr@gmail.com>
2026-04-08 00:31:04 +0200

    Added script to test deployment.

---
+++ scripts/debug_flatpak.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+
+# ./scripts/build_flatpak.sh && flatpak install --user --reinstall build/nui-sftp.flatpak --include-sdk --include-debug && flatpak run org.nuicpp.nui_sftp --enable-dev-tools
+
+flatpak run --command=bash org.nuicpp.nui_sftp
