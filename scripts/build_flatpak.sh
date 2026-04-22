7d7f808e99ec8972bc200e0a45d2243527c20709
5cript <tim06tr@gmail.com>
2026-04-22 23:24:19 +0000

    bla

--- scripts/build_flatpak.sh
+++ scripts/build_flatpak.sh
@@ -9,7 +9,8 @@ source "${SCRIPT_DIR}/lib.sh"
 SOURCE_DIR="${SOURCE_DIRECTORY:-${SCRIPT_DIR}/..}"
 SOURCE_DIR=$(canonicalPath "${SOURCE_DIR}")
 
-flatpak-builder --force-clean --install-deps-from=flathub build/flatpak org.nuicpp.nui_sftp.yml
+# flatpak-builder --user --install-deps-from=flathub build/flatpak org.nuicpp.nui_sftp.yml
+flatpak-builder --user --force-clean --install-deps-from=flathub build/flatpak org.nuicpp.nui_sftp.yml
 flatpak build-export build/flatpak-export build/flatpak
 flatpak build-bundle build/flatpak-export build/nui-sftp.flatpak org.nuicpp.nui_sftp --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
 
--- scripts/debug_flatpak.sh
+++ scripts/debug_flatpak.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 
-# ./scripts/build_flatpak.sh && flatpak install --user --reinstall build/nui-sftp.flatpak --include-sdk --include-debug && flatpak run org.nuicpp.nui_sftp --enable-dev-tools
+./scripts/build_flatpak.sh && flatpak install --user --reinstall build/nui-sftp.flatpak --include-sdk --include-debug
 
 # flatpak install --user --reinstall build/nui-sftp.flatpak
 
-flatpak run --command=bash org.nuicpp.nui_sftp
+# flatpak run --command=bash org.nuicpp.nui_sftp
